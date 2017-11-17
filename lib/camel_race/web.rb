41fd503c10baf19806b837cbedf8724c27d8d844
Jose Galisteo <ceritium@gmail.com>
2017-11-17 08:37:25 +0100

    Make it mountable, url helpers copied from Resque
    https://github.com/resque/resque/blob/686bc8ae2018baa080536f6a4f5bb49975a99dda/lib/resque/server.rb#L43

--- lib/camel_race/web.rb
+++ lib/camel_race/web.rb
@@ -13,6 +13,21 @@ module CamelRace
       def partial(page, options = {})
         erb page, options.merge!(layout: false)
       end
+
+      def url_path(*path_parts)
+        [url_prefix, path_prefix, path_parts].join("/").squeeze("/")
+      end
+
+      alias_method :u, :url_path
+
+      def path_prefix
+        request.env["SCRIPT_NAME"]
+      end
+
+      # In the future it will be a configuration param
+      def url_prefix
+        nil
+      end
     end
 
     get "/" do
