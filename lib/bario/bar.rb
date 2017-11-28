63e900007c7bb8f8dc7f1db9e256fe297dee8d2a
Jose Galisteo <ceritium@gmail.com>
2017-11-28 19:26:46 +0100

    Bar get return a null bar

--- lib/bario/bar.rb
+++ lib/bario/bar.rb
@@ -2,6 +2,8 @@
 
 require "forwardable"
 require "bario/inspector"
+require "bario/null_bar"
+require "bario/storage"
 
 module Bario
   # Bar encapsulate all the logic to create, list, find, delete and update
@@ -18,19 +20,24 @@ module Bario
 
     class << self
       def find(id)
-        bar = InternalBar[id]
+        bar = Storage[id]
         bar ? new(bar) : nil
       end
 
+      def get(id)
+        bar = Storage[id]
+        bar ? new(bar) : NullBar.new
+      end
+
       def all
-        collection(InternalBar.find(root: true))
+        collection(Storage.find(root: true))
       end
 
       def create(opts = {})
         opts[:total] ||= DEFAULT_TOTAL
         opts[:root] = true unless opts.key?(:root)
 
-        bar = InternalBar.create(opts)
+        bar = Storage.create(opts)
 
         parent = opts[:parent]
         parent.children.push(bar) if parent
@@ -103,54 +110,5 @@ module Bario
     private
 
     attr_reader :bar
-
-    # Internal storage backed in redis for bar
-    class InternalBar < Ohm::Model
-      attribute :name
-      index :name
-
-      attribute :root
-      index :root
-
-      attribute :total
-      attribute :current
-      counter :current
-
-      list :children, "Bario::Bar::InternalBar"
-      reference :parent, "Bario::Bar::InternalBar"
-
-      # Extracted from ohm-contrib
-      Timestamp = ->(time) { time && time.to_i.to_s }
-      attribute :created_at, Timestamp
-      attribute :updated_at, Timestamp
-
-      class << self
-        def time_now
-          Time.now.utc.to_i
-        end
-      end
-
-      def save
-        time_now = self.class.time_now
-
-        self.created_at = time_now if new?
-        self.updated_at = time_now
-        super
-      end
-
-      # Override increment update `updated_at` attribute
-      # `decrement` call to `increment`.
-      def increment(att, count = 1)
-        time_now = self.class.time_now
-
-        redis.queue("HINCRBY", key[:counters], att, count)
-        redis.queue("HSET", key, "updated_at", time_now)
-
-        self.updated_at = time_now
-        redis.commit
-      end
-    end
-
-    private_constant :InternalBar
   end
 end
